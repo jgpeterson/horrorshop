f3de6ae33d235233d77424567f34eec620066898
jamespeterson <jamespeterson0918@yahoo.com>
2017-12-15 14:13:20 -0500

    added more figure seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ Figure.destroy_all
 
 Figure.create({name: "https://i.imgur.com/YIWAO6n.jpg", description: "Sweet dreams. Having haunted our dreams (nightmares) for well over three decades, it only made sense that the next classic horror villain to be realized as a Sideshow Sixth Scale Figure would be Freddy Krueger.", link: "https://www.amazon.com/Sideshow-Nightmare-Collectibles-Krueger-Collectible/dp/B0759NS7G3/ref=sr_1_2?ie=UTF8&qid=1513191312&sr=8-2&keywords=horror+sideshow+collectibles"})
 Figure.create({name: "https://i.imgur.com/SHzXOJa.jpg", description: "Accessories include bloody machete, neck restraint with chains, wrist restraint with chains, mask and leather straps", link: "https://www.amazon.com/Sideshow-Collectibles-Freddy-Krueger-Englund/dp/B0007Y20L6/ref=sr_1_3?ie=UTF8&qid=1513191312&sr=8-3&keywords=horror+sideshow+collectibles"})
+Figure.create({name: "https://i.imgur.com/tXvp5F7.jpg", description: "The Texas Chainsaw Massacre - Thomas Hewitt aka Leatherface. Includes: Chainsaw / Axe / Apron / Base", link: "https://www.amazon.com/Sideshow-Collectibles-Chainsaw-Massacre-Leatherface/dp/B001LNGFJU/ref=sr_1_3?ie=UTF8&qid=1513364898&sr=8-3&keywords=leatherface+sideshow"})
 
 Board.destroy_all
 
