aae89355ef069b56075205275d92007aa9ea04f2
jamespeterson <jamespeterson0918@yahoo.com>
2017-12-20 14:20:41 -0500

    set up homepage for good

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Figures from './components/Figures'
 import Boards from './components/Boards'
 import NewBoardForm from './components/NewBoardForm'
+import HomePage from './components/HomePage'
 
 class App extends Component {
   render () {
@@ -10,6 +11,7 @@ class App extends Component {
       <Router>
         <div>
           <Switch>
+            <Route path="/" component={HomePage}/>
             <Route path="/figures" component={Figures}/>
             <Route path="/boards" component={Boards}/>
           </Switch>
