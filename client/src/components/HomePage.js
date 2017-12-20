f01cc574ee185a30bdd783f3c993aa2a40981993
jamespeterson <jamespeterson0918@yahoo.com>
2017-12-20 14:38:22 -0500

    added a background

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+
+
 
 class HomePage extends Component {
     render() {
         return (
+
+            
             <div>
-                
+             
             </div>
         );
     }
